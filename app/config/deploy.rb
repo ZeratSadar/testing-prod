263d3cf792da44bf4bd8751b7374af09846fa7c3
ZeratSadar <desvignes.theo@gmail.com>
2016-02-19 17:01:30 +0100

    deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,10 +1,14 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :application, "Testing"
+set :domain,      "digoneiojc@ftp.cluster005.ovh.net"
+set :user,        "digoneiojc"
+set :use_sudo,    false
+set :deploy_to,   "/homez.2193/digoneiojc/www/test"
 set :app_path,    "app"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "git@host:ZeratSadar/testing-prod.git"
+set :branch, "master"
 set :scm,         :git
+set :deploy_via,  :copy
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
@@ -15,5 +19,18 @@ role :app,        domain, :primary => true       # This may be the same as your 
 
 set  :keep_releases,  3
 
+## Symfony2
+set :shared_files, ["app/config/parameters.yml"] # Les fichiers à conserver entre chaque déploiement
+set :shared_children, [app_path + "/logs", "vendor"] # Idem, mais pour les dossiers
+set :use_composer, true
+set :update_vendors, false # Il est conseillé de laisser a false et de ne pas faire de ‘composer update’ directement sur la prod
+#set :composer_options, "--verbose --prefer-dist" # Permet de spécifier des paramètres supplémentaires à composer, inutile dans notre cas
+set :writable_dirs, ["app/cache", "app/logs"] # Application des droits nécessaires en écriture sur les dossiers
+set :webserver_user, "www-data" # L’utilisateur de votre serveur web (Apache, nginx, etc.)
+set :permission_method, :chown # Dans le cas où vous n’avez pas les ACLs, ne pas oublier de mettre :use_sudo à true
+set :use_set_permissions, true
+set :dump_assetic_assets, true # dumper les assets
+
+logger.level = Logger::MAX_LEVEL
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
