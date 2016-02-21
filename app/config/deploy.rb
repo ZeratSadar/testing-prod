f6cb7b6bc47b1a765786e34efd52baa4c21f6d97
ZeratSadar <desvignes.theo@gmail.com>
2016-02-21 14:24:41 +0100

    testing deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,12 +1,9 @@
 set :application, "Testing"
 set :domain,      "digoneiojc@ftp.cluster005.ovh.net"
-set :user,        "digoneiojc"
-set :use_sudo,    false
 set :deploy_to,   "/homez.2193/digoneiojc/www/test"
 set :app_path,    "app"
 
-set :repository,  "git@host:ZeratSadar/testing-prod.git"
-set :branch, "master"
+set :repository,  "git@github.com:ZeratSadar/testing-prod.git"
 set :scm,         :git
 set :deploy_via,  :copy
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
@@ -15,21 +12,18 @@ set :model_manager, "doctrine"
 # Or: `propel`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :app,        domain # This may be the same as your `Web` server
+role :db,         domain, :primary => true
 
+set :use_sudo, false
+set :use_composer, true
 set  :keep_releases,  3
 
 ## Symfony2
-set :shared_files, ["app/config/parameters.yml"] # Les fichiers à conserver entre chaque déploiement
-set :shared_children, [app_path + "/logs", "vendor"] # Idem, mais pour les dossiers
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
 set :use_composer, true
-set :update_vendors, false # Il est conseillé de laisser a false et de ne pas faire de ‘composer update’ directement sur la prod
-#set :composer_options, "--verbose --prefer-dist" # Permet de spécifier des paramètres supplémentaires à composer, inutile dans notre cas
-set :writable_dirs, ["app/cache", "app/logs"] # Application des droits nécessaires en écriture sur les dossiers
-set :webserver_user, "www-data" # L’utilisateur de votre serveur web (Apache, nginx, etc.)
-set :permission_method, :chown # Dans le cas où vous n’avez pas les ACLs, ne pas oublier de mettre :use_sudo à true
-set :use_set_permissions, true
-set :dump_assetic_assets, true # dumper les assets
+set :update_vendors, true
 
 logger.level = Logger::MAX_LEVEL
 # Be more verbose by uncommenting the following line
